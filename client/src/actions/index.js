e3e77edb9887210c96699f6940d9bd1de049c582
RubenSantana <ruben_santana1990@hotmail.com>
2019-07-15 15:45:44 +0100

    Added the client Article View

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -16,3 +16,35 @@ export function getBooks(limit = 10, start = 0, order = "asc", list = "") {
     payload: request
   };
 }
+
+export function getBookWithReviewer(id) {
+  const request = axios.get(`/api/getBook?id=${id}`);
+
+  return dispatch => {
+    request.then(({ data }) => {
+      let book = data;
+
+      axios.get(`/api/getReviewer?id=${book.ownerId}`).then(({ data }) => {
+        let response = {
+          book,
+          reviewer: data
+        };
+
+        dispatch({
+          type: "GET_BOOK_W_REVIEWER",
+          payload: response
+        });
+      });
+    });
+  };
+}
+
+export function clearBookWithReviewer() {
+  return {
+    type: "CLEAR_BOOK_W_REVIEWER",
+    payload: {
+      book: {},
+      reviewer: {}
+    }
+  };
+}
